99fe07b79a832fc653d4c3c03d481d3adaacaca4
VAARJAA <VAARJAA@WL-D39T9S3>
2023-03-28 15:11:34 +0300

    Add project files.

---
+++ Jaakko Empower 1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Jaakko Empower 1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Jaakko Empower 1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("AB148702-F038-4D6C-A013-0EAFE0141987")]
+[assembly: AssemblyVersion("1.0.0.0")]
